a8c3a7c162450aea5cada213e303a53b6a957760
adrianonobre <adrianonobre@mpf.mp.br>
2025-05-21 16:18:27 -0300

    Dashboard inicial

---
+++ frontend/src/app/pedidos/pedidos-gerenciador/pedidos-gerenciador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-gerenciador',
+  imports: [],
+  templateUrl: './pedidos-gerenciador.component.html',
+  styleUrl: './pedidos-gerenciador.component.scss'
+})
+export class PedidosGerenciadorComponent {
+
+}
