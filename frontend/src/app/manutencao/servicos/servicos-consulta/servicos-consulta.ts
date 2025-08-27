6d4091b1b9c2af69f37d416d0397663ed09dcade
adrianonobre <adrianonobre@mpf.mp.br>
2025-08-27 15:48:15 -0300

    Aula 10 - Crud Serviços - Edição

--- frontend/src/app/manutencao/servicos/servicos-consulta/servicos-consulta.ts
+++ frontend/src/app/manutencao/servicos/servicos-consulta/servicos-consulta.ts
@@ -1,17 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DsCrudImports } from '@dsmpf/ngx-dsmpf/crud';
-import { DsDatatableColunaDef } from '@dsmpf/ngx-dsmpf/datasource/datatable';
+import { DsCrudConsultaComponent, DsCrudImports, DsCrudPadraoService } from '@dsmpf/ngx-dsmpf/crud';
+import { DsDatatableAcaoDef, DsDatatableColunaDef, DsDatatableImports } from '@dsmpf/ngx-dsmpf/datasource/datatable';
 import { DsFormImports } from '@dsmpf/ngx-dsmpf/form';
 import { Servico } from '../../../shared/model/servico';
 import { Categoria } from '../../../shared/model/categoria';
+import { DsFormSwitchDirective } from '@dsmpf/ngx-dsmpf/form/switch';
 
 @Component({
   selector: 'app-servicos-consulta',
   imports: [
     FormsModule,
     DsCrudImports,
-    DsFormImports
+    DsFormImports,
+    DsDatatableImports,
+    DsFormSwitchDirective
   ],
   templateUrl: './servicos-consulta.html',
   styleUrl: './servicos-consulta.scss'
@@ -20,10 +23,50 @@ export class ServicosConsulta {
 
   listagemCategorias = input<Categoria[]>([]) // está obtendo do resolve da configuração do Crud
 
+  protected crudApi = inject(DsCrudPadraoService<Servico>);
+
+  protected crudConsulta = viewChild.required(DsCrudConsultaComponent<Servico>);
+
   protected colunas: DsDatatableColunaDef<Servico>[] = [
     { id: 'categoria.nome', ordenavel: true, titulo: 'Categoria', largura: '30%' },
     { id: 'nome', ordenavel: true, titulo: 'Nome' },
-    { id: 'ativo', ordenavel: true, titulo: 'Ativo', largura: '100', traducaoBoolean: {valorTrue: 'Sim', valorFalse: 'Não'} }
+    // { id: 'ativo', ordenavel: true, titulo: 'Ativo', largura: '100', traducaoBoolean: {valorTrue: 'Sim', valorFalse: 'Não'} }
   ];
+
+
+  protected acoesDatatable: DsDatatableAcaoDef<Servico>[] = [
+    {
+      id: 'ativar',
+      descricao: 'Ativar o serviço',
+      icone: 'la-check',
+      cor: 'success',
+      recarregar: true,
+      funcao: (servico) => this.alterarStatusServico(servico, true),
+      ocultar: (servico) => servico.ativo
+    },
+    {
+      id: 'desativar',
+      descricao: 'Desativar o serviço',
+      icone: 'la-ban',
+      cor: 'danger',
+      recarregar: true,
+      funcao: (servico) => this.alterarStatusServico(servico, false),
+      ocultar: (servico) => !servico.ativo
+    },
+  ];
+
+
+  protected inverterStatusServico(servico: Servico, event: Event) {
+    event.preventDefault();
+
+    this.crudConsulta().datatable().processarAcaoAvulsa({
+      funcao: () => this.alterarStatusServico(servico, !servico.ativo),
+      recarregar: true
+    });
+  }
+
+  private alterarStatusServico(servico: Servico, ativo: boolean) {
+    return this.crudApi.alterar({...servico, ativo});
+  }
 
 }
--- frontend/src/app/manutencao/servicos/servicos-edicao/servicos-edicao.ts
+++ frontend/src/app/manutencao/servicos/servicos-edicao/servicos-edicao.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DsCrudImports } from '@dsmpf/ngx-dsmpf/crud';
+import { DsFormImports } from '@dsmpf/ngx-dsmpf/form';
+import { Categoria } from '../../../shared/model/categoria';
+import { Servico } from '../../../shared/model/servico';
 
 @Component({
   selector: 'app-servicos-edicao',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    DsCrudImports,
+    DsFormImports
+  ],
   templateUrl: './servicos-edicao.html',
   styleUrl: './servicos-edicao.scss'
 })
 export class ServicosEdicao {
+
+  listagemCategorias = input<Categoria[]>([]) // está obtendo do resolve da configuração do Crud
+
+  private fb = new FormBuilder();
+
+  protected formServicos = this.fb.group({
+    'nome': this.fb.control<string>('', {
+      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(100)]
+    }),
+    'descricao': this.fb.control<string>('', {
+      validators: [Validators.minLength(10), Validators.maxLength(200)]
+    }),
+    'ativo': this.fb.control<boolean>(true, {nonNullable: true}),
+    'idCategoria': this.fb.control<number | null>(null, {
+      validators: [Validators.required]
+    })
+  });
+
+
+  protected tratamentoCarregar = (servico: Servico) => {
+    return {
+      ...servico,
+      idCategoria: servico.categoria?.id
+    };
+  }
+
+  protected tratamentoSalvar = (valor: any) => {
+    // recupera o objeto da categoria antes de salvar
+    const categoria = this.listagemCategorias().find(c => c.id === valor.idCategoria);
+    valor.idCategoria = undefined; // descarta este atributo, existente apenas no formulário
+
+    return {
+      ...valor,
+      categoria
+    } as Servico;
+  }
 
 }
